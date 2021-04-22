2f3b475b5013b6b108ccc412eaaad805d0d158c2
Sabboshachi Sarkar <55670542+Sabboshachi-Sarkar@users.noreply.github.com>
2021-04-22 23:56:03 +0600

    codeUpdate

---
+++ Even and Odd Number check.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+num = input("Enter a Mumber:")
+num = int(num)
+
+num_1 = 2
+
+if num % 2 == 0:
+    print ('THe number is even')
+else:
+    print("The number is odd")
+
